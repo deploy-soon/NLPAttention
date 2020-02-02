1b6a5aa146931e290d84fb0d2d9539057547d0d1
soon <dyd3312@gmail.com>
2020-02-02 22:27:00 +0900

    model structure

--- model.py
+++ model.py
@@ -1,19 +1,65 @@
+import fire
+import pathlib
+import numpy as np
+from os.path import join as pjoin
+
 from keras.models import Model, Sequential
 from keras.layers import GRU, Input, Dense, TimeDistributed, Activation, RepeatVector, Bidirectional, Dropout, LSTM
 from keras.layers.embeddings import Embedding
 from keras.optimizers import Adam
 from keras.losses import sparse_categorical_crossentropy
+from keras import backend as K
+from keras.callbacks import ModelCheckpoint, TensorBoard
 
+
+from misc import get_logger
 from data import TranslationData
 
-class Model:
 
-    def __init__(self, learning_rate=0.005):
+class TranslationModel:
+    """
+    1. rnn, lstm, gru, bdlstm, attention
+    """
+
+    def __init__(self, learning_rate, batch_size, epochs, validation_rate):
         self.data = TranslationData()
         self.data.load()
         self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.validation_rate = validation_rate
+        self._feed_forward = None
 
-    def _simple_gru(self, input_shape):
+    def build(self):
+        raise NotImplementedError
+
+    def feed_forward(self, x):
+        if self._feed_forward is not None:
+            raise NotImplementedError
+        inference = self._feed_forward([x.reshape(1, -1, 1), 0])
+        return inference.reshape(-1) # flatten
+
+    def feed_forward_batch(self, x):
+        if self._feed_forward is not None:
+            raise NotImplementedError
+        inference = self._feed_forward([x, 0])
+        return inference
+
+    def run(self):
+        net = self.build()
+        print(net.summary())
+        net.fit(x=self.data.en_sentences,
+                y=self.data.fr_sentences,
+                batch_size=self.batch_size,
+                epochs=self.epochs,
+                validation_split=self.validation_rate)
+        print(self.data.logits_to_text(net.predict(self.data.en_sentences[:1])[0]))
+
+
+class GRU(Model):
+
+    def build(self):
+        input_shape = self.data.en_sentences.shape
         output_dim_size = len(self.data.fr_tk.word_index)
         model = Sequential()
         model.add(GRU(256, input_shape=input_shape[1:], return_sequences=True))
@@ -21,31 +67,18 @@ class Model:
         model.add(Dropout(0.5))
         model.add(TimeDistributed(Dense(output_dim_size + 1, activation='softmax')))
 
+        # model = Model()
         model.compile(loss=sparse_categorical_crossentropy,
                       optimizer=Adam(self.learning_rate),
                       metrics=['accuracy'])
+        self._feed_forward = K.function([model.input, K.learning_phase()],
+                                        [model.get_layer("logits").output])
         return model
-
-    def build(self, input_shape):
-        _model = self._simple_gru(input_shape=input_shape)
-        return _model
 
     def test(self):
         pass
 
-    def run(self):
-        _model = self.build(self.data.en_sentences.shape)
-        print(_model.summary())
-        _model.fit(x=self.data.en_sentences,
-                   y=self.data.fr_sentences,
-                   batch_size=1024,
-                   epochs=5,
-                   validation_split=0.3)
-        print(self.data.logits_to_text(model.predict(self.data.en_sentences[:1])[0]))
 
 if __name__ == "__main__":
     model = Model()
     model.run()
-
-
-
